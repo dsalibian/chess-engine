64f7bbfc18bf94c3d905cd49b14c389ce531e46a
dantesall <dante.salibian@gmail.com>
2024-06-08 20:17:28 -0700

    add constants

--- src/position.h
+++ src/position.h
@@ -5,14 +5,19 @@ using namespace std;
 typedef unsigned long long ull;
 typedef unsigned short ushort;
 
-const ull a_file = 0x0101010101010101ULL;
-const ull b_file = 0x0202020202020202ULL;
-const ull g_file = 0x4040404040404040ULL;
-const ull h_file = 0x8080808080808080ULL;
-const ull rank_1 = 0x00000000000000ffULL;
-const ull rank_2 = 0x000000000000ff00ULL;
-const ull rank_7 = 0x00ff000000000000ULL;
-const ull rank_8 = 0xff00000000000000ULL;
+const ull a_file  = 0x0101010101010101ULL;
+const ull b_file  = 0x0202020202020202ULL;
+const ull g_file  = 0x4040404040404040ULL;
+const ull h_file  = 0x8080808080808080ULL;
+const ull ab_file = 0x303030303030303ULL;
+const ull gh_file = 0xc0c0c0c0c0c0c0c0ULL;
+
+const ull rank_1  = 0x00000000000000ffULL;
+const ull rank_2  = 0x000000000000ff00ULL;
+const ull rank_7  = 0x00ff000000000000ULL;
+const ull rank_8  = 0xff00000000000000ULL;
+const ull rank_12 = 0x000000000000ffffULL;
+const ull rank_78 = 0xffff000000000000ULL;
 
 enum Squares{ 
     a1, b1, c1, d1, e1, f1, g1, h1, 
