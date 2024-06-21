cc9a63cc9ea44a04e230b18993fa0cce98db859c
ds <dante.salibian@gmail.com>
2024-06-21 01:16:11 -0700

    fix bit_count and lsb_index

--- src/misc.h
+++ src/misc.h
@@ -9,8 +9,8 @@
     __builtin_clzll()   Count Leading Zeros
     __builtin_ctzll()   Count Trailing Zeros
 */
-#define bit_count(n) __builtin_popcountll(n);
-#define lsb_index(n) __builtin_ctzll(n); 
+#define bit_count(n) __builtin_popcountll(n)
+#define lsb_index(n) __builtin_ctzll(n) 
 
 using namespace std;
 typedef unsigned long long ull;
@@ -20,5 +20,6 @@ bool get_bit(ull, int);
 void set_bit(ull&, int, bool);
 int pop_lsb(ull&);
 ull BB(int);
+int f(ull, ull, int);
 
 #endif
