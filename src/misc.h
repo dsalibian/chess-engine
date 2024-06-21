8d3f615d0d655c08b48e1a46af915d2fbe3153d1
ds <dante.salibian@gmail.com>
2024-06-21 16:40:24 -0700

    add function timer

--- src/misc.h
+++ src/misc.h
@@ -2,6 +2,7 @@
 #define MISC_H
 
 #include <iostream>
+#include <chrono>
 
 /*
     __builtin_popcountll() - count 1 bits
@@ -21,5 +22,12 @@ void set_bit(ull&, int, bool);
 int pop_lsb(ull&);
 ull BB(int);
 int f(ull, ull, int);
+
+template <typename F>
+long time_func(F& f) {
+    auto t0 = chrono::steady_clock::now();
+    f();    
+    return chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now()-t0).count();
+}
 
 #endif
