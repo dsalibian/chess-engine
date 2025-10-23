a0afed6ed3bba6982cbb1c764ec925769eda451b
dsalibian <dsalibian@proton.me>
2025-10-22 20:23:15 -0700

    add types.h

---
+++ src/types.h
@@ -0,0 +1,31 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t  i8;
+
+typedef u64 bitboard;
+
+enum square {
+    A1, B1, C1, D1, E1, F1, G1, H1,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A8, B8, C8, D8, E8, F8, G8, H8,
+};
+
+
+#endif
