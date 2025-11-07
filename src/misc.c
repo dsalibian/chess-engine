4bd7f09efe7bdfa72c483549874777dd2c83b388
dsalibian <dsalibian@proton.me>
2025-11-06 22:25:57 -0800

    remove old function

--- src/misc.c
+++ src/misc.c
@@ -33,24 +33,3 @@ void board_print(const u8* board, const bitboard w_bb) {
 
     printf("\n     a b c d e f g h\n\n");
 }
-
-
-bool sqr_attd_opps(
-        const struct atts_tbl* tbl, 
-        const u32 sqr, 
-        const bitboard all, 
-        const bitboard* opps, 
-        const bool turn)
-{
-    const bitboard batts = bmoves_bb(tbl, sqr, all, 0);
-    const bitboard ratts = rmoves_bb(tbl, sqr, all, 0);  
-    const bitboard qatts = batts | ratts;
-
-    return 
-        (opps[IDX_PAWN]  & tbl->patts[turn][sqr]) ||
-        (opps[IDX_NIGHT] & tbl->natts[sqr]) ||
-        (opps[IDX_KING]  & tbl->katts[sqr]) ||
-        (opps[IDX_BISHP] & batts) || 
-        (opps[IDX_ROOK]  & ratts) || 
-        (opps[IDX_QUEEN] & qatts); 
-}
